4a47c34b1c077192cebc8a62c0dc065e0d76eb94
Gnapy Daubrey <daubreyrael@gmail.com>
2025-10-25 22:22:01 +0000

    Lesson 10, communication event

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -10,6 +10,8 @@ const props = defineProps({
   }
 })
 
+const emit = defineEmits(['add-to-cart'])
+
 const product = ref('Socks')
 const brand = ref('Vue Mastery')
 
@@ -43,7 +45,10 @@ const shipping = computed(() => {
   }
 })
 
-const addToCart = () => cart.value += 1
+const addToCart = () => {
+  emit('add-to-cart', variants.value[selectedVariant.value].id)
+}
+//const addToCart = () => cart.value += 1
 
 const updateVariant = (index) => {
   selectedVariant.value = index
